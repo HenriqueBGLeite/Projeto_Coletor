8a564188aa3fa840b88367292693a7967320e96c
Henrique Batista <henrique.batista@epoca.dc>
2019-08-06 16:34:21 -0300

    Feat: Implementa Serviço Front

--- src/app/componentes/pesquisa-produto/pesquisa-produto.service.ts
+++ src/app/componentes/pesquisa-produto/pesquisa-produto.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PesquisaProdutoService {
 
-  constructor() { }
+  urlApi = environment.urlApi.concat('buscaProduto');
+
+  constructor(private httpClient: HttpClient) { }
+
+  public buscarProduto(id: string){
+    return this.httpClient.get(`${this.urlApi}/${id}`);
+  }
 }
